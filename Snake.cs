5872ad454a7ad9eb65605772dec7ce63409539a5
бека турсункулов <tursunkulovbeka@gmail.com>
2022-08-21 16:59:53 +0600

    Snake

---
+++ Snake.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NuniTeam2
+{
+
+    public class Snake
+    {
+        public int BodyLong {get; set;}
+
+        public int Speed {get; set;}
+
+        public void Move(){
+            
+        }
+
+        public void TernLeft(){
+
+        }
+
+        public void TernRight(){
+
+        }
+    }
+}
