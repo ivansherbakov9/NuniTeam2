6b78f5931142409cec13b25d1049614ee3bc47ed
DenisKH06 <deniskh04@gmail.com>
2022-08-21 20:34:20 +0300

    sfa

--- GenerateApples.cs
+++ GenerateApples.cs
@@ -4,22 +4,23 @@ namespace NuniTeam2
 {
     public class GenerateApples
     {
-        MapDrawer mapDrawer;
+        MapDrawer mapDrawer = new MapDrawer();
         List<int> range =  new List<int> {7,8,9,10,13,14,15,16,19,20,21,22,25,26,27};
-        public bool spawn = true;
+        static public bool spawn = true;
         public GenerateApples()
         {
-            mapDrawer = new MapDrawer();
-            if (spawn)
-                Spawn();
-
+           Spawn();
         }
         public void Spawn()
         {
-            Random rnd = new Random();
-            int num = rnd.Next(7,29);
-            int rangeRnd = rnd.Next(0,range.Count);
-            mapDrawer.DrawApple(range[rangeRnd],0);
+            if (spawn)
+            {
+                Random rnd = new Random();
+                int num = rnd.Next(7,29);
+                int rangeRnd = rnd.Next(0,range.Count);
+                mapDrawer.DrawApple(range[rangeRnd],0);
+            }
+            
         }
 
     }
--- MapDrawer.cs
+++ MapDrawer.cs
@@ -9,8 +9,6 @@ namespace NuniTeam2
     public class MapDrawer
     {
 
-        
-
         //private char[,] map = new char[16, 16];
         static string [,] map = {{"* "},{"* "},{"* "},{"* "},{"* "},{"* \n"},
                                   {"* "},{"  "},{"  "},{"  "},{"  "},{"* \n"},
@@ -37,17 +35,17 @@ namespace NuniTeam2
         }
         public void DrawApple(int x, int y)
         {
-            GenerateApples _generateApples = new GenerateApples();
+            
             if (map[x,y] != "# ")
             {
                 map[x,y] = "@ ";
-            }    
-            else
+                GenerateApples.spawn = false;
+            }
+            else 
             {
-                if (_generateApples.spawn)
-                    _generateApples.Spawn();
-                _generateApples.spawn = false;
+                GenerateApples _generateApples = new GenerateApples();
             }    
+          
             foreach(string j in map)
             {
                 Console.Write(j);
@@ -55,7 +53,7 @@ namespace NuniTeam2
         }
         public MapDrawer()
         {
-            MapClear();
+            
         }
     }
 }
